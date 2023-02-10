f82aa56dec35c02c4ef3c7c43842272ef8d73257
arivusurya <arivusurya.3@gmail.com>
2023-02-10 07:54:57 +0530

    montly wages

--- Dailwages.cs
+++ Dailwages.cs
@@ -1,27 +1,36 @@
 namespace EmployeWage {
 
     public class dailywage{
+        public const int number_ofWorking_days=20;
+
 
         public static void wagecalculation(){
             int employeehour = 0;
             int wageperhour = 20 ;
+            int totalemployeewage =0; 
+           for(int day =0 ;day <number_ofWorking_days;day++){
             Random random = new Random();
-            int empcheck = random.Next(3);
-            if (empcheck == 0 ){
-                employeehour = 8;
-            }else if(empcheck == 1){
+           int number = random.Next(3);
+           switch (number)
+           {
+            case 0:
+                employeehour =8;
+                break;
+            case 1:
                 employeehour =4;
-            }else{
+                break;
+            case 2:
                 employeehour =0;
-            }
+                break;
+            
+           }
 
             int dailywageofworker = employeehour * wageperhour;
-
+            totalemployeewage +=dailywageofworker;
             Console.WriteLine("The daily wage is {0}",dailywageofworker);
+           }
+           Console.WriteLine("Total employee wage for a month is "+totalemployeewage);
         }
 
     }
-
-
-
 }
