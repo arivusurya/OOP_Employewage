972ad4e31d3bf436b527a4db72ed9a46942dd7aa
arivusurya <arivusurya.3@gmail.com>
2023-02-12 16:20:39 +0530

    daily wages

---
+++ wages.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace EmployeWage {
+
+    public class wages{
+
+            public static void dailywage(){
+                int workperhour = 20 ;
+                int workinghour = 8;
+                int dailywage = workinghour * workperhour;
+                Console.WriteLine(" daily wage is {0} ",dailywage );
+            }
+
+    }
+
+}
